9d173ae743614d32b83024488ade6448439f3ee0
Scott <Tortel1210@gmail.com>
2011-10-24 14:44:38 -0400

    Enabled ext4 support, and init.d scripts

--- initramfs_eh17/sbin/custom_init.sh
+++ initramfs_eh17/sbin/custom_init.sh
@@ -43,26 +43,9 @@ then
 fi
 sync
 
-# keyboard patch sysfs call 5 for snappy keyboard performance (range: 1-16 default: 7)
-if [ ! -f "/data/local/timer_delay" ]; then
-  echo 5 > /data/local/timer_delay
-fi
-cat /data/local/timer_delay > /sys/devices/platform/s3c-keypad/timer_delay
+# set permissions for su
+chmod 6755 /system/sbin/su
 
-# setup tmpfs for su
-mkdir /sdx
-mount -o size=30k -t tmpfs tmpfs /sdx
-cat /sbin/su > /sdx/su
-chmod 6755 /sdx/su
-# establish root in common system directories for 3rd party applications
-rm /system/bin/su
-rm /system/xbin/su
-rm /system/bin/jk-su
-ln -s /sdx/su /system/bin/su
-ln -s /sdx/su /system/xbin/su
-# remove su in problematic locations
-rm -rf /bin/su
-rm -rf /sbin/su
 
 #setup proper passwd and group files for 3rd party root access
 # Thanks DevinXtreme
@@ -75,30 +58,11 @@ if [ ! -f "/system/etc/group" ]; then
 	chmod 0666 /system/etc/group
 fi
 
-#Check and setup Superuser if missing
- if [ ! -f "/system/app/Superuser.apk" ] && [ ! -f "/data/app/Superuser.apk" ] && [[ ! -f "/data/app/com.noshufou.android.su"* ]]; then
-	if [ -f "/system/app/Asphalt5_DEMO_ANMP_Samsung_D700_Sprint_ML.apk" ]; then
-		rm /system/app/Asphalt5_DEMO_ANMP_Samsung_D700_Sprint_ML.apk
-	fi
-	if [ -f "/system/app/FreeHDGameDemos.apk" ]; then
-		rm /system/app/FreeHDGameDemos.apk
-	fi
- 	busybox cp /sbin/Superuser.apk /system/app/Superuser.apk
- fi
-
 # fix busybox DNS while system is read-write
 if [ ! -f "/system/etc/resolv.conf" ]; then
 	echo "nameserver 8.8.8.8" > /system/etc/resolv.conf
 	echo "nameserver 8.8.4.4" >> /system/etc/resolv.conf
 fi 
-
-# Patch to attempt removal and prevention of DroidDream malware
-if [ -f "/system/bin/profile" ]; then
-	rm /system/bin/profile
-fi
-touch /system/bin/profile
-chmod 644 /system/bin/profile
-sync
 
 # remount read only and continue
 mount -o remount,ro /dev/block/stl9 /system
